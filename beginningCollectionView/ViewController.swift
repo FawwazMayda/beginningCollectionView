74be2d298165a47791493f5a2d7216ffeec4f11c
fawwazmayda <maydafawwaz@gmail.com>
2020-05-22 16:01:01 +0800

    Initial Commit

---
+++ beginningCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beginningCollectionView
+//
+//  Created by Muhammad Fawwaz Mayda on 22/05/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
